19a69f801fe1ac8ec12e188bb5f4087383101d7b
lim jian han <jianhanl98@gmail.com>
2024-07-03 22:25:29 +0800

    convert to c for speed processing

---
+++ app/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+setup(
+    ext_modules = cythonize(
+        Extension(
+            "main",
+            sources=["main.py"],
+        )
+    )
+)
